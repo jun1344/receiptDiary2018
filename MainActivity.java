531c7320c76930bd35196c4035b2af02ff1f78ce
junsic <jun12041344@gmail.com>
2018-05-11 18:19:31 +0900

    button Event

--- MainActivity.java
+++ MainActivity.java
@@ -2,15 +2,23 @@ package com.example.junsic.receiptdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+    }
+
+    protected class NetSocket extends Thread{
+
+        
+
 
     }
 }
